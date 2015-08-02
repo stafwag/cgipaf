ebf182c5a3a8a5657d655035ee6610df935d5c99
Staf Wagemakers <staf@wagemakers.be>
2015-08-02 11:51:35 +0200

    removed old salt code

--- tstsalt.c
+++ tstsalt.c
@@ -14,13 +14,14 @@ int main() {
 
 	salt=crypt_make_salt("DES",NULL);
 	printf("new salt DES: %s %d\n",salt,strlen(salt));
-
+/*
 	salt=md5_seed();
 	printf("old salt md5: %s %d\n",salt,strlen(salt));
 
 	salt=std_seed();
 	printf("old salt DES: %s %d\n",salt,strlen(salt));
 
+*/
 
 	salt=crypt_make_salt("SHA256",&sha_rounds);
 	printf("new salt SHA256: %s %d\n",salt,strlen(salt));

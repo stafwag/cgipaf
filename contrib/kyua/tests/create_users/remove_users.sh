e29821cb9465818c9065eb6472e62313423a152a
Staf Wagemakers <staf@wagemakers.be>
2017-08-28 13:56:01 +0200

    create_users.sh added

---
+++ contrib/kyua/tests/create_users/remove_users.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#
+# removes the test users
+#
+# Copyright (C) 2017 Staf Wagemakers Belgie/Belgium
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+#
+
+while read userLine; do
+
+        echo $userLine | grep -E "^tst..\:" > /dev/null && {
+
+                user=`echo $userLine | cut -f 1 -d \:`
+
+                echo "Removing $user"
+
+                userdel $user
+
+
+        }
+
+done < /etc/passwd
+
